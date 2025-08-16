079dd3b35517440930ecfd2da1141cdd3f547036
pasparsw <pbar.mail94@gmail.com>
2025-08-16 23:46:45 +0200

    Analog read with PWM example ready

--- arduino.py
+++ arduino.py
@@ -12,6 +12,12 @@ OUTPUT: int = 0
 INPUT: int = 1
 INPUT_PULLUP: int = 2
 LED_BUILTIN: int = 0
+A0 = 0
+A1 = 1
+A2 = 2
+A3 = 3
+A4 = 4
+A5 = 5
 
 def pinMode(pin: int, mode: int) -> None:
     pass
@@ -126,14 +132,17 @@ class Arduino:
                 function_body_end_line = len(python_code)
 
                 for i in range(line_number + 1, len(python_code)):
-                    current_num_of_tabs: int = int((len(python_code[i]) - len(python_code[i].lstrip(" ")))/4)
+                    current_num_of_tabs: int = int((len(python_code[i]) - len(python_code[i].lstrip(" "))) / 4)
 
                     if current_num_of_tabs < num_of_tabs and i != len(python_code):
-                        next_num_of_tabs: int = int((len(python_code[i + 1]) - len(python_code[i + 1].lstrip(" ")))/4)
+                        if python_code[i] == "\n":
+                            next_num_of_tabs: int = int((len(python_code[i + 1]) - len(python_code[i + 1].lstrip(" "))) / 4)
 
-                        if next_num_of_tabs < num_of_tabs:
-                            function_body_end_line = i
-                            break
+                            if next_num_of_tabs >= num_of_tabs:
+                                continue
+
+                        function_body_end_line = i
+                        break
 
                 current_indentation: str = indentation + "    "
                 cpp_code += f"{function_return_type} {function_name}({function_arguments})\n{{\n"
@@ -141,16 +150,45 @@ class Arduino:
                 cpp_code += "}\n"
                 line_number = function_body_end_line
                 continue
+            # for loop
+            elif "for " in python_line and " in range(" in python_line:
+                loop_range: str = python_line.split(" in range(")[1].split(")")[0]
+                cpp_code += f"{indentation}for (i=0; i<{loop_range}; i++)\n{indentation}{{\n"
+                num_of_tabs: int = int((len(python_line) - len(python_line.lstrip(" "))) / 4) + 1
+                for_loop_body_end_line = len(python_code)
+
+                for i in range(line_number + 1, len(python_code)):
+                    current_num_of_tabs: int = int((len(python_code[i]) - len(python_code[i].lstrip(" "))) / 4)
+
+                    if current_num_of_tabs < num_of_tabs and i != len(python_code):
+                        if python_code[i] == "\n":
+                            next_num_of_tabs: int = int(
+                                (len(python_code[i + 1]) - len(python_code[i + 1].lstrip(" "))) / 4)
+
+                            if next_num_of_tabs >= num_of_tabs:
+                                continue
+
+                        for_loop_body_end_line = i
+                        break
+
+                current_indentation: str = indentation + "    "
+                cpp_code += self.__parse_block(python_code[line_number + 1:for_loop_body_end_line], current_indentation)
+                cpp_code += f"{indentation}}}\n"
+                line_number = for_loop_body_end_line
+                continue
             # function call
             elif "(" in python_line and ")" in python_line:
                 cpp_code += f"{indentation}{python_line.lstrip()};\n"
             # variable definition
             elif ": " in python_line and " = " in python_line:
                 variable_name: str = python_line.split(":")[0]
-                variable_type: str = python_line.split(": ")[1].split(" =")[0]
                 variable_value: str = python_line.split("= ")[1]
+                variable_type: str = python_line.split(": ")[1].split(" =")[0]
 
-                cpp_code += f"{indentation}{variable_type} {variable_name.lstrip()} = {variable_value};\n"
+                if variable_name.isupper():
+                    cpp_code += f"#define {variable_name.lstrip()} {variable_value}\n"
+                else:
+                    cpp_code += f"{indentation}{variable_type} {variable_name.lstrip()} = {variable_value};\n"
             # variable assignment
             elif " = " in python_line:
                 variable_name: str = python_line.split(" =")[0]
@@ -178,7 +216,7 @@ class Arduino:
                         if python_code[i] == "\n":
                             next_num_of_tabs: int = int((len(python_code[i + 1]) - len(python_code[i + 1].lstrip(" "))) / 4)
 
-                            if next_num_of_tabs > num_of_tabs:
+                            if next_num_of_tabs >= num_of_tabs:
                                 continue
 
                         if_statement_body_end_line = i
--- blink.py
+++ blink.py
@@ -1,28 +1,11 @@
 from arduino import *
 
 with Arduino("some port", "uno"):
-    counter: int = 0
-
     def setup() -> None:
-        global counter
-
-        Serial.begin(9600)
         pinMode(LED_BUILTIN, OUTPUT)
-        counter = 12
 
     def loop() -> None:
-        global counter
-
         digitalWrite(LED_BUILTIN, HIGH)
         delay(1000)
         digitalWrite(LED_BUILTIN, LOW)
         delay(1000)
-
-        counter += 1
-
-        if counter < 15:
-            Serial.println("Counter lower than 15")
-        if counter == 15:
-            Serial.println("Counter equal to 15")
-        if counter > 15:
-            Serial.println("Counter greater than 15")
